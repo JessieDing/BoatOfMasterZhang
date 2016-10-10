2ab60783a3c67dfe93f5909ec6bd4f2371e09d0f
Jessie Ding <nathalie.ding@gmail.com>
2016-10-10 21:05:41 +0800

    add break

--- src/main/java/com/company/boatAcrossArches.java
+++ src/main/java/com/company/boatAcrossArches.java
@@ -6,27 +6,26 @@ public class boatAcrossArches {
 
     public static void main(String[] args) {
         Scanner scan1 = new Scanner(System.in);
-        System.out.println("第一条水路桥洞高度：");
+        System.out.println("a路桥洞高度：");
         String road1 = scan1.nextLine();
         int a = Integer.parseInt(road1);
         Scanner scan2 = new Scanner(System.in);
-        System.out.println("第二条水路桥洞高度：");
+        System.out.println("b路桥洞高度：");
         String road2 = scan2.nextLine();
         int b = Integer.parseInt(road2);
         Scanner scan3 = new Scanner(System.in);
-        System.out.println("第三条水路桥洞高度：");
+        System.out.println("c路桥洞高度：");
         String road3 = scan3.nextLine();
         int c = Integer.parseInt(road3);
         if (a < 50 && b < 50 && c < 50) {
             System.out.println("IMPASSABLE");
         }
-        //if (a>=50||b>=50||c>=50){
         int arr[] = {a, b, c};
         for (int i = 0; i < 3; i++) {
             if (arr[i] >= 50) {
-                System.out.println("PASS " + "桥洞高度："+arr[i]);
+                System.out.println("PASS " + "首条可通过水路的桥洞高度：" + arr[i]);
+                break;//??break放在哪里
             }
         }
-
     }
 }

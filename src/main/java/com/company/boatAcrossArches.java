ff28c21fbb9fac435a080773bdd11168c2bc63ef
Jessie Ding <nathalie.ding@gmail.com>
2016-10-10 21:27:47 +0800

    Refactor

--- src/main/java/com/company/boatAcrossArches.java
+++ src/main/java/com/company/boatAcrossArches.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class boatAcrossArches {
 
     public static void main(String[] args) {
-        Scanner scan1 = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);  //只需要一个扫描器，获得三次输入
         System.out.println("a路桥洞高度：");
-        String road1 = scan1.nextLine();
-        int a = Integer.parseInt(road1);
-        Scanner scan2 = new Scanner(System.in);
+        String inputHeight1 = scan.nextLine();  //nextLine获取下一行的输入数据
+        int archHeight1 = Integer.parseInt(inputHeight1);
+
         System.out.println("b路桥洞高度：");
-        String road2 = scan2.nextLine();
-        int b = Integer.parseInt(road2);
-        Scanner scan3 = new Scanner(System.in);
+        String inputHeight2 = scan.nextLine();
+        int archHeight2 = Integer.parseInt(inputHeight2);
+
         System.out.println("c路桥洞高度：");
-        String road3 = scan3.nextLine();
-        int c = Integer.parseInt(road3);
-        if (a < 50 && b < 50 && c < 50) {
+        String inputHeight3 = scan.nextLine();
+
+        int archHeight3 = Integer.parseInt(inputHeight3);
+        if (archHeight1 < 50 && archHeight2 < 50 && archHeight3 < 50) {
             System.out.println("IMPASSABLE");
         }
-        int arr[] = {a, b, c};
+        int arr[] = {archHeight1, archHeight2, archHeight3};
         for (int i = 0; i < 3; i++) {
             if (arr[i] >= 50) {
-                System.out.println("PASS " + "首条可通过水路的桥洞高度：" + arr[i]);
+                System.out.println("PASS " + "首条可过船水路的桥洞高度：" + arr[i]);
                 break;//??break放在哪里
             }
         }

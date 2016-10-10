57779dfb3aa5a3b96800837c81dae59c286a6c13
Jessie Ding <nathalie.ding@gmail.com>
2016-10-10 20:21:16 +0800

    init project

---
+++ src/main/java/com/company/boatAcrossArches.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class boatAcrossArches {
+
+    public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("第一条水路桥洞高度：");
+        String road1 = scan1.nextLine();
+        int a = Integer.parseInt(road1);
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("第二条水路桥洞高度：");
+        String road2 = scan2.nextLine();
+        int b = Integer.parseInt(road2);
+        Scanner scan3 = new Scanner(System.in);
+        System.out.println("第三条水路桥洞高度：");
+        String road3 = scan3.nextLine();
+        int c = Integer.parseInt(road3);
+
+    }
+}
